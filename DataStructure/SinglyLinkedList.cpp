ceaaefb54c09d338ce58257e67b58a44a19db4c7
Rohit Nagtilak <rohitnagtilak337@gmail.com>
2023-09-17 13:27:46 +0530

    Modified

--- DataStructure/SinglyLinkedList.cpp
+++ DataStructure/SinglyLinkedList.cpp
@@ -1,273 +1,226 @@
-// DataStructure.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Node
 {
 public:
-    int data;
-    Node* next;
-     
-    Node():data(0),next(NULL) {}
+	int data;
+	Node* next;
 
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
+	Node() :data(0), next(NULL) {}
+
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
 };
 
 Node* createNode()
 {
-    int data;
-    cout << "Enter data to store : ";
-    cin >> data;
+	int data;
+	cout << "\nEnter data to store:";
+	cin >> data;
 
-    Node* node = new Node(data);
+	Node* node = new Node(data);
 
-    return node;
+	return node;
+}
+
+Node* InsertAtStart(Node* head)
+{
+	Node* node = createNode();
+	if (head == NULL)
+		head = node;
+	else
+	{
+		node->next = head;
+		head = node;
+	}
+	return head;
+}
+
+Node* InsertAtEnd(Node* head)
+{
+	Node* node = createNode();
+	if (head == NULL)
+		head = node;
+	else
+	{
+		Node* temp = head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = node;
+	}
+
+	return head;
 }
 
 
-Node* AddNodeAtStart(Node* head)
+Node* InsertAtMiddle(Node* head)
 {
-    Node* node = createNode();
+	if (head == NULL)
+	{
+		cout << "List is empty. Cannot insert at middle." << endl;
+		return head;
+	}
 
-    if (head == NULL)
-    {
-        head = node;
-    }
-    else
-    {
-        node->next = head;
-        head = node;
-    }
-    return head;
+	int key;
+	cout << "Enter key after which you want to add data: ";
+	cin >> key;
+
+	Node* node = createNode();
+	Node* temp = head;
+	while (temp != NULL)
+	{
+		if (temp->data == key)
+		{
+			node->next = temp->next;
+			temp->next = node;
+			cout << "Node inserted successfully." << endl;
+			break;
+		}
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+		cout << "Node with " << key << " data not found." << endl;
+
+	return head;
 }
 
-Node* AddNodeAtEnd(Node* head)
+
+Node* DeleteFromStart(Node* head)
 {
-    Node* node = createNode();
-    Node* temp = head;
-
-    if (head == NULL)
-        head = node;
-    else
-    {
-        while (temp->next != NULL)
-            temp = temp->next;
-
-        temp->next = node;
-    }
-
-    return head;
+	if (head == NULL)
+	{
+		cout << "LinkedList is empty"<<endl;
+	}
+	else
+	{
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
+	return head;
 }
 
-Node* AddNodeAtMiddle(Node* head)
+Node* DeleteFromEnd(Node* head)
 {
-    Node* temp = head;
-    if (head == NULL)
-    {
-        cout << "Linked List is Empty.\nAdd First Node...!"<<endl;
-        head= createNode();
-    }
-    else
-    {
-        int key;
-        cout << "\n Enter element after which you want to add data :"<<endl;
-        cin >> key;
+	if (head == NULL)
+	{
+		cout << "\nList is empty..!"<<endl;
+		return head;
+	}
+	else if (head->next == NULL)
+	{
+		delete head;
+		head = NULL;
+		return head;
+	}
+	else
+	{
+		Node* temp = head;
+		while (temp->next->next != NULL)
+			temp = temp->next;
 
-        Node* newnode = createNode();
-        if (head->next == NULL)
-        {
-            if (head->data == key)
-            {
-                head->next = newnode;
-            }
-            else
-            {
-                cout << "Node with " << key << " data not found." << endl;
-            }
-        }
-        else
-        {
-            while (temp!=NULL)
-            {
-                if (temp->data == key)
-                {
-                    newnode->next = temp->next;
-                    temp->next = newnode;
-                    break;
-                }
-                else
-                    temp = temp->next;
-            }
-            if(temp==NULL)
-                cout << "Node with " << key << " data not found." << endl;
-        }
-    }
-    return head;
-}
-                                    
-Node* DeleteFromStart(Node *head)
-{
-    Node* temp = head;
-    if (head == NULL)
-        cout << "LinkedList is empty";
-    else
-    {
-        head = head->next;
-        delete(temp);
-    }
-    return head;
-}
-                                    
-Node* DeleteFromEnd(Node *head)
-{
-    Node* temp = head;
-    if (head == NULL)
-        cout << "LinkedList is empty";
-    else if (head->next == NULL)
-    {
-        delete(temp);
-        temp = NULL;
-        head = NULL;
-    }
-    else
-    {
-        while (temp->next->next != NULL)
-            temp = temp->next;
-
-        delete(temp->next);
-        temp->next = NULL;
-    }
-    return head;
+		delete temp->next;
+		temp->next = NULL;
+		return head;
+	}
 }
 
 Node* DeleteFromMiddle(Node* head)
 {
-    Node* temp = head;
-    if (head == NULL)
-    {
-        cout << "Linked List is Empty." << endl;
-    }
-    else
-    {
-        int key;
-        cout << "\n Enter data which you want to delete :" << endl;
-        cin >> key;
-        if (head->next == NULL)
-        {
-            if (head->data == key)
-            {
-                delete(temp);
-                temp = NULL;
-                head = NULL;
-            }
-            else
-            {
-                cout << "Node with " << key << " data not found." << endl;
-            }
-        }
-        else
-        {
-            while (temp != NULL)
-            {
-                if (temp->next->data == key)
-                {
-                    Node *a = temp->next;
-                    temp->next = temp->next->next;
-                    delete(a);
-                    break;
-                }
-                else
-                    temp = temp->next;
-            }
-            if (temp == NULL)
-                cout << "Node with " << key << " data not found." << endl;
-        }
-    }
-    return head;
+	if (head == NULL)
+	{
+		cout << "\nList is empty..!\n";
+		return head;
+	}
+
+	int key;
+	cout << "\nEnter data which you want to delete : ";
+	cin >> key;
+
+	if (head->data == key)
+	{
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+		return head;
+	}
+
+	Node* temp = head;
+	while (temp->next != NULL)
+	{
+		if (temp->next->data == key)
+		{
+			Node* t = temp->next;
+			temp->next = temp->next->next;
+			delete t;
+			return head;
+		}
+		temp = temp->next;
+	}
+
+	cout << key << " not found..!\n";
+	return head;
 }
 
-void DisplayLinkedList(Node *head)
+
+void Display(Node* head)
 {
-    Node* temp =  head;
-    if(head == NULL)
-    {
-        cout << "Linked List Is Empty.!" << endl;
-    }
-    else
-    {
-        cout << "--------------------------------------------" << endl;
-        while (temp != NULL)
-        {
-            cout << " " << temp->data << " ->";
-            temp = temp->next;
-        }
-        cout << "\n--------------------------------------------" << endl;
-    }
+	if (head == NULL)
+		cout << "\n List is empty...!";
+	else
+	{
+		Node* temp = head;
+		while (temp != NULL)
+		{
+			cout << " " << temp->data << " ->";
+			temp = temp->next;
+		}
+	}
 }
 
 int main()
 {
-    Node *head = NULL;
-    while(1)
-    {
-        cout << "\n----------- Singly Linked List -----------" << endl;
-        cout << "   1. Create First Node" << endl;
-        cout << "   2. Add Node At The Start" << endl;
-        cout << "   3. Add Node At The Middle" << endl;
-        cout << "   4. Add Node At The End"<<endl;
-        cout << "   5. Display List" << endl;
-        cout << "   6. Delete From Start" << endl;
-        cout << "   7. Delete From End" << endl;
-        cout << "   8. Delete From Middle" << endl;
-        cout << "-----------------------------------------" << endl;
-
-        int choice;
-        cout << "Enter Your Choice :- " << endl;
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            head = createNode();
-            break;
-        case 2:
-            head = AddNodeAtStart(head);
-            break;
-        case 3:
-            head = AddNodeAtMiddle(head);
-                break;
-        case 4:
-            head = AddNodeAtEnd(head);
-            break;
-        case 5:
-                   DisplayLinkedList(head);
-            break;
-        case 6:
-            head = DeleteFromStart(head);
-            break;
-        case 7:
-            head = DeleteFromEnd(head);
-            break;
-        case 8:
-            head = DeleteFromMiddle(head);
-            break;
-        default:
-            break;
-        }
-    }
-
-    Node* temp = head;
-    while (temp != NULL)
-    {
-        Node* nextNode = temp->next;
-        delete temp;
-        temp = nextNode;
-    }
-    return 0;
+	Node* head = NULL;
+	while (1)
+	{
+		cout << "\n 1. Create List\n 2. Insert At Start\n 3. Insert At End\n 4. Insert At Middle\n 5. Delete from start \n 6. Delete from end \n 7. Delete from middle \n 8. Display";
+		int ch;
+		cout << "\nEnter your choice:";
+		cin >> ch;
+		switch (ch)
+		{
+		case 1:
+			head = createNode();
+			break;
+		case 2:
+			head = InsertAtStart(head);
+			break;
+		case 3:
+			head = InsertAtEnd(head);
+			break;
+		case 4:
+			head = InsertAtMiddle(head);
+			break;
+		case 5:
+			head = DeleteFromStart(head);
+			break;
+		case 6:
+			head = DeleteFromEnd(head);
+			break;
+		case 7:
+			head = DeleteFromMiddle(head);
+			break;
+		case 8:
+			Display(head);
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
 }
